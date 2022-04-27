b42d977759b14529cad00e769d1ecdca90b5e442
Yash Saxena <70855054+yashsaxena7999@users.noreply.github.com>
2022-04-27 21:23:00 +0530

    Add files via upload

---
+++ Fibonacci_Series.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the number of elements till which you want to find the Fibonacci Series : "))
+x=0
+b=1
+if(n==1):
+    print(x)
+else:
+    print(x)
+    print(b)
+    for i in range(2,n):
+        temp=x+b
+        x=b
+        b=temp
+        print(temp)
+        
+    
